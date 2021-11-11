dc5ca34331c9c70c317a8ca46b7bc74f048dbc8d
pater97 <giuseppepater997@gmail.com>
2021-11-11 14:56:08 +0100

    header completato

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- header -->
+    <header id="site_header">
+      <siteHeader/>
+    </header>
+    <!-- main -->
+    <main id="main_site">
+
+    </main>
+    <!-- footer -->
+    <footer id="site_footer">
+
+    </footer>
+  </div>
+</template>
+
+<script>
+import siteHeader from './components/siteHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    siteHeader
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  
+}
+</style>
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <section class="container">
+            <div class="logo">
+                <img src="../assets/dc-logo.png" alt="">
+            </div>
+            <nav class="nav_choise">
+                <a href="#">CHARATERS</a>
+                <a href="#">COMICS</a>
+                <a href="#">MOVIES</a>
+                <a href="#">TV</a>
+                <a href="#">GAMES</a>
+                <a href="#">COLLECTIBLES</a>
+                <a href="#">VIDEOS</a>
+                <a href="#">FANS</a>
+                <a href="#">NEWS</a>
+                <a href="#">SHOP</a>
+            </nav>
+        </section>
+    </div>
+</template>
+
+<style lang="scss">
+        @import '/variabili.scss';
+    .container{
+        width: $generalWidth; 
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 2rem;
+        img{
+            width: 5rem;
+        }
+        a{
+            text-decoration: none;
+            color: black;
+            padding: 1rem;
+            font-weight: 700;
+            &:hover{
+                color: $brandColor;
+                border-bottom: 3px solid $brandColor;
+            }
+        }
+    }
+</style>
